41c14143066eb0bafc27f30040e31f3aa157610e
Byron Panimboza <byron.115@hotmail.com>
2026-01-19 11:21:09 +0100

    fix: all images unoptimized

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: process.env.NODE_ENV === "development" || !process.env.VERCEL,
+    unoptimized: true
   }
 };
 
